b450ef354abaa2547aabdd7c5ec353c6a64cf578
w-son <williamson@naver.com>
2020-05-24 17:32:59 +0900

    연관관계 테스트

--- src/main/java/com/son/SpringJPA/domain/Member.java
+++ src/main/java/com/son/SpringJPA/domain/Member.java
@@ -2,19 +2,38 @@ package com.son.SpringJPA.domain;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity @Builder
 @Getter @Setter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA는 기본적으로 파라미터가 없는 생성자를 사용해야할 수 있어야 한다
 @AllArgsConstructor
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id") // 데이터베이스에서 FK 매핑할 때 쓸 것
     private Long id;
 
     private String username;
+
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 
 }
---
+++ src/main/java/com/son/SpringJPA/domain/Team.java
@@ -0,0 +1,28 @@
+package com.son.SpringJPA.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity @Builder
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
